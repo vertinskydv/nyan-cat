7f999a8621b2743af7b6476851ac497368172f12
Dmitry Vertinsky <vertinskydv@gmail.com>
2017-02-03 17:52:36 +0300

    nyan-cat/feature: add canvas field

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,25 @@
-() => {console.log('@@@')};
+class Game {
+
+  constructor () {
+    var canvas = document.getElementById('canvas');
+    this.ctx = canvas.getContext('2d');
+    this.drawCanvas();
+    this.startAnimation();
+  }
+
+  drawCanvas() {
+    this.ctx.fillStyle = "#104476";
+    this.ctx.fillRect(0, 0, 960, 600);
+  }
+
+  startAnimation () {
+    requestAnimationFrame(main);
+  }
+
+
+
+}
+
+function bodyOnload() {
+  let game = new Game();
+}
