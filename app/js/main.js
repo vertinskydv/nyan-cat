1d0e129e46ec81bb7b97eec5b258a085ecbe778d
Dmitry Vertinsky <vertinskydv@gmail.com>
2017-04-02 20:43:23 +0300

    nyan-game/feature: add svg cat

--- app/js/main.js
+++ app/js/main.js
@@ -4,19 +4,29 @@ class Game {
     var canvas = document.getElementById('canvas');
     this.ctx = canvas.getContext('2d');
     this.drawCanvas();
-    this.startAnimation();
+    this.getSprites();
+
   }
 
   drawCanvas() {
-    this.ctx.fillStyle = "#104476";
+    this.ctx.fillStyle = "#104476"; //game field background color
     this.ctx.fillRect(0, 0, 960, 600);
   }
 
-  startAnimation () {
-    requestAnimationFrame(main);
+  getSprites() {
+    var Cat = new Image();
+    var c = this.ctx;
+    Cat.width = 34;
+    Cat.height = 21;
+    Cat.onload = function() {
+      c.drawImage(Cat, 10, 10);
+    };
+    Cat.src = './img/nyan-svg/nyan1.svg';
+
+
+
+
   }
-
-
 
 }
 
